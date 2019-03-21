addb51bc57ab1dabc249f9645b2b4c1663f8bd56
Junyoung McArdle <juny.mcardle@gmail.com>
2019-03-21 00:32:41 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,12 +3,7 @@ require 'pry'
 def my_find(collection)
   i = 0
   while i < collection.length
-    binding.pry
     return collection[i] if yield(collection[i])
     i = i + 1
   end
 end
-
-
-collection = (1..15).to_a
-my_find(collection) {|i| i % 3 == 0 and i % 5 == 0 }
